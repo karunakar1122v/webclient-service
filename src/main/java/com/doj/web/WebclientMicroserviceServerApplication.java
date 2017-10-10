a1e80b3f922814c4785c6b70779bb052245017f4
karunakar1122v <karunakar_v@infosys.com>
2017-10-10 06:45:58 +0000

    Update WebclientMicroserviceServerApplication.java

--- src/main/java/com/doj/web/WebclientMicroserviceServerApplication.java
+++ src/main/java/com/doj/web/WebclientMicroserviceServerApplication.java
@@ -2,13 +2,13 @@ package com.doj.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class WebclientMicroserviceServerApplication {
 	
 	public static final String ACCOUNTS_SERVICE_URL = "http://ACCOUNTS-MICROSERVICE";
